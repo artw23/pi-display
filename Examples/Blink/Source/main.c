1052393e76f7cf78e7f553879d74ace708aa2222
Arturo Rivera <arrivera@localhost.localdomain>
2018-08-13 22:43:11 -0500

    Add debug statments

--- Examples/Blink/Source/main.c
+++ Examples/Blink/Source/main.c
@@ -3,26 +3,29 @@
 
 int main()
 {
-	if(map_peripheral(&gpio) == -1) 
+	if(map_peripheral(&gpio) == -1)
 	{
        	 	printf("Failed to map the physical GPIO registers into the virtual memory space.\n");
         	return -1;
     	}
 
 	// Define gpio 17 as output
+	printf("Defining GPIO 17 as output");
 	INP_GPIO(17);
 	OUT_GPIO(17);
 
 	while(1)
 	{
 		// Toggle 17 (blink a led!)
+		printf("ON!!");
 		GPIO_SET = 1 << 17;
 		sleep(1);
 
+		printf("OFF!!");
 		GPIO_CLR = 1 << 17;
 		sleep(1);
 	}
 
-	return 0;	
+	return 0;
 
 }

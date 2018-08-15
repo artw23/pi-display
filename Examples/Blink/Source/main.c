0d2d85ab6e86db72dd3865fa3cd47bf7a5acf5b8
Arturo Rivera <arrivera@localhost.localdomain>
2018-08-15 07:29:38 -0500

    Add interruption

--- Examples/Blink/Source/main.c
+++ Examples/Blink/Source/main.c
@@ -31,13 +31,18 @@ int nums[10][7] = { { 0,0,0,0,0,0,1 },    // 0
                     { 0,0,0,0,0,0,0 },    // 8
                     { 0,0,0,1,1,0,0 }};   // 9
 
-int actualCount = 0;
+#define BUTTON_0 21
+#define BUTTON_1 22
+#define BUTTON_2 23
+#define BUTTON_3 24
 
-PI_THREAD (displayThread){
-  for(;;){
-    displayNumber(actualCount);
-  }
-}
+enum mode{RUNNING, EDIT_DIGIT_0, EDIT_DIGIT_1, EDIT_DIGIT_2, EDIT_DIGIT_3};
+int actualDigits[4] = {0,0,0,0};
+int actualCount = 0;
+enum mode actualMode = RUNNING;
+
+
+
 
 void displaySingleNum(int num){
   int *actualNum = nums[num];
@@ -65,8 +70,102 @@ void displayNumber(int number){
     number = number /10;
     enableDigit(i);
     displaySingleNum(toDisplay);
-    delay(3);
+    delay(5);
   }
+}
+
+PI_THREAD (displayThread){
+  for(;;){
+    displayNumber(actualCount);
+  }
+}
+
+void waitForBounce(){
+  delay(300);
+}
+
+void button0Interrupt(void){
+  printf("Interrupt 0\n");
+  waitForBounce();
+  if(actualMode == RUNNING){
+    actualMode = EDIT_DIGIT_0;
+  }else{
+    actualMode = RUNNING;
+  }
+}
+
+void button1Interrupt(void){
+  printf("Interrupt 1\n");
+  waitForBounce();
+  if(actualMode == RUNNING){
+    return;
+  }
+  if(actualMode == EDIT_DIGIT_0){
+    actualMode = EDIT_DIGIT_1;
+  }else if(actualMode == EDIT_DIGIT_1){
+    actualMode = EDIT_DIGIT_2;
+  }else if(actualMode == EDIT_DIGIT_2){
+    actualMode = EDIT_DIGIT_3;
+  }else{
+    actualMode = EDIT_DIGIT_0;
+  }
+}
+
+
+int editCount(){
+  return actualDigits[0] * 1000 + actualDigits[1] * 100 + actualDigits[2] * 10 + actualDigits[3];
+}
+
+void button2Interrupt(void){
+  printf("Interrupt 2\n");
+  waitForBounce();
+  if(actualMode == RUNNING){
+    return;
+  }
+  if(actualMode == EDIT_DIGIT_0){
+    actualDigits[0] = (actualDigits[0] + 1) % 10;
+  }else if(actualMode == EDIT_DIGIT_1){
+      actualDigits[1] = (actualDigits[1] + 1) % 10;
+  }else if(actualMode == EDIT_DIGIT_2){
+      actualDigits[2] = (actualDigits[2] + 1) % 10;
+  }else{
+      actualDigits[3] = (actualDigits[3] + 1) % 10;
+  }
+  printf("Editing %i\n", editCount());
+}
+
+void button3Interrupt(void){
+  printf("Interrupt 3\n");
+  waitForBounce();
+  if(actualMode == RUNNING){
+    return;
+  }
+  if(actualMode == EDIT_DIGIT_0){
+    int newDigit = (actualDigits[0] - 1);
+    if(newDigit < 0){
+      newDigit = 9;
+    }
+    actualDigits[0] = newDigit;
+  }else if(actualMode == EDIT_DIGIT_1){
+    int newDigit = (actualDigits[1] - 1);
+    if(newDigit < 0){
+      newDigit = 9;
+    }
+    actualDigits[1] = newDigit;
+  }else if(actualMode == EDIT_DIGIT_2){
+    int newDigit = (actualDigits[2] - 1);
+    if(newDigit < 0){
+      newDigit = 9;
+    }
+    actualDigits[2] = newDigit;
+  }else{
+    int newDigit = (actualDigits[3] - 1);
+    if(newDigit < 0){
+      newDigit = 9;
+    }
+    actualDigits[3] = newDigit;
+  }
+  printf("Editing %i\n", editCount());
 }
 
 
@@ -98,6 +197,26 @@ int main (void)
   wiringPiSetup () ;
 	initDisplays();
 
+  if ( wiringPiISR (BUTTON_0, INT_EDGE_FALLING, &button0Interrupt) < 0 ) {
+      printf("Error creating interruption...\n");
+      return 1;
+  }
+
+  if ( wiringPiISR (BUTTON_1, INT_EDGE_FALLING, &button1Interrupt) < 0 ) {
+      printf("Error creating interruption...\n");
+      return 1;
+  }
+
+  if ( wiringPiISR (BUTTON_2, INT_EDGE_FALLING, &button2Interrupt) < 0 ) {
+      printf("Error creating interruption...\n");
+      return 1;
+  }
+
+  if ( wiringPiISR (BUTTON_3, INT_EDGE_FALLING, &button3Interrupt) < 0 ) {
+      printf("Error creating interruption...\n");
+      return 1;
+  }
+
   printf("Starting main loop...\n");
   int count = 0;
   int print = 0;
@@ -105,8 +224,17 @@ int main (void)
   piThreadCreate (displayThread) ;
   for (;;)
   {
-    actualCount --;
-    delay(1000);
+    switch (actualMode) {
+      case RUNNING:
+        if(actualCount>0){
+          actualCount --;
+        }
+        delay(1000);
+      break;
+      default:
+        actualCount = editCount();
+      break;
+    }
   }
   return 0 ;
 }

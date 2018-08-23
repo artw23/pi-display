acc287b6f75103ff8ca79aee951f43a6a267ec73
Arturo Rivera <arrivera@localhost.localdomain>
2018-08-22 20:34:41 -0500

    test another split

--- Examples/Sound/Source/main.c
+++ Examples/Sound/Source/main.c
@@ -15,7 +15,7 @@ const float notes[] = {16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 23.12, 24.50, 2
 
 #define MAMA_MIA "32f2 32#d2 32f2 8#d2 32#d2 32#d2 32f2 32g2 32f2 16.#d2 32- 16f2 8#d2 16#g2 32#g2 32#g2 32#g2 16g2 16#d232- 8#a2 32#a2 32#a2 16#a2 16f2 16g2 8#g2 16g2 16g2 32g2 16g2 16d2 16#d2 8f2 16f2 8#d2 16#g2 32#g2 32#g232#g2 32g2 32#d2 32f2 16#d2"
 
-#define HIGHWAY "4#d1 4#d1 8#d1 4- 4- 4- 4- 4c1 4c1 8#c1 4- 4- 4- 4- 4c1 4c1 2#c1 4c1 4c1 2#c1 2c1 4#d1 2#d1"
+#define HIGHWAY "4#d1 4#d1 8#d1 4- 4- 4- 4- 4c1 4c1 8#c1 4- 4- 4- 4- 4c1 4c1 2#c1 4c1 4c1 2#c1 2c1 4#c1 2#c1"
 
 #define NOKIA "8e2 8d2 4#f2 4#f2 4e2 4d2 2#f2 8d2 8e2 4d2 2d2 8a1 8b1 4d2 4e2 4#c2 4b1 8e2 8d2 4#f2 4#f2 4e2 4d2 2#f2 8d2 8e2 4d2 2d2 8a1 8b1 4d2 4e2 4#c2 4b1 8a1 8a1"
 char * ACTUAL_SONG = HIGHWAY;

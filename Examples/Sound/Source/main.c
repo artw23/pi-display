28234ebb508c04616fff09ddb8709a81b3c9a216
Arturo Rivera <arrivera@localhost.localdomain>
2018-08-22 20:48:57 -0500

    test another split

--- Examples/Sound/Source/main.c
+++ Examples/Sound/Source/main.c
@@ -9,42 +9,6 @@
 
 const int DEBUG = 1;
 
-
-//Pins that enable each one of the 4 displays
-const int digits[4] =
-{
-  26, // Enable digit 0
-  28, // Enable digit 1
-  27, // Enable digit 2
-  29  // Enable digit 3
-};
-
-//Pins for the segments of the display
-
-const int segments[7] =
-{
-  2, // Segment A
-  0, // Segment B
-  6, // Segment C
-  5, // Segment D
-  4, // Segment E
-  7, // Segment E
-  1  // Segment F
-};
-
-// Values for each segment to represent the numbers from 0 to 9.
-
-const int nums[10][7] = { { 0,0,0,0,0,0,1 },    // 0
-                    { 1,0,0,1,1,1,1 },    // 1
-                    { 0,0,1,0,0,1,0 },    // 2
-                    { 0,0,0,0,1,1,0 },    // 3
-                    { 1,0,0,1,1,0,0 },    // 4
-                    { 0,1,0,0,1,0,0 },    // 5
-                    { 0,1,0,0,0,0,0 },    // 6
-                    { 0,0,0,1,1,1,1 },    // 7
-                    { 0,0,0,0,0,0,0 },    // 8
-                    { 0,0,0,1,1,0,0 }};   // 9
-
 const float notes[] = {16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 23.12, 24.50, 25.96, 27.50, 29.14, 30.97};
 
 #define BACK_IN_BLACK "8e1 4- 4- 4- 4- 4d2 4d2 8d2 4- 4- 4- 4- 4#c2 4#c2 8#c2 4- 4- 4- 4- 4g2 4e2 4d2 4b1 4b1 4a1 4g1 8e1"
@@ -57,30 +21,6 @@ const float notes[] = {16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 23.12, 24.50, 2
 
 #define NOKIA "8e2 8d2 4#f2 4#f2 4e2 4d2 2#f2 8d2 8e2 4d2 2d2 8a1 8b1 4d2 4e2 4#c2 4b1 8e2 8d2 4#f2 4#f2 4e2 4d2 2#f2 8d2 8e2 4d2 2d2 8a1 8b1 4d2 4e2 4#c2 4b1 8a1 8a1"
 char * ACTUAL_SONG = MISION_IMPOSIBLE;
-
-void displaySingleNum(int num){
-  int *actualNum = nums[num];
-  int i;
-  for(i = 0; i < 7; i++){
-    digitalWrite (segments[i], actualNum[i]);
-  }
-}
-
-// Enable one of the 4 displays
-void enableAll(){
-  int i;
-  for(i =0; i< 4; i++){
-    digitalWrite (digits[i], HIGH);
-  }
-}
-
-
-void disableAll(){
-  int i;
-  for(i =0; i< 4; i++){
-    digitalWrite (digits[i], LOW);
-  }
-}
 
 int split (const char *str, char c, char ***arr)
 {
@@ -209,31 +149,11 @@ void playNote(char *note, int scale, int duration){
     }
     softToneWrite (BUZZER, getScaleNote(note, scale));
     int realDelay = 2000/duration;
-    disableAll();
     delay(realDelay);
-    enableAll();
 }
 
 void stop(){
   softToneWrite (BUZZER, 0);
-}
-
-void initDisplays(){
-    int i, total;
-
-    // Init enable displays
-    total = sizeof(digits)/sizeof(digits[0]);
-    for(i =0; i < total; i++){
-      pinMode (digits[i], OUTPUT) ;
-      digitalWrite (digits[i], HIGH) ;	// On
-    }
-
-    // Init segments of diplays
-    total = sizeof(segments)/sizeof(segments[0]);
-    for(i =0; i < total; i++){
-      pinMode (segments[i], OUTPUT) ;
-
-    }
 }
 
 int main (void)
@@ -241,8 +161,8 @@ int main (void)
   printf ("Raspberry Pi buzzer\n") ;
   wiringPiSetup () ;
   softToneCreate(BUZZER);
-  initDisplays();
-  displaySingleNum(0);
+
+
 
   char **arr = NULL;
 

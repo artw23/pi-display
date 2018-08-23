d47d08a6b339e7f14601dc44211489cbfe11dbe8
Arturo Rivera <arrivera@localhost.localdomain>
2018-08-22 20:05:12 -0500

    test another split

--- Examples/Sound/Source/main.c
+++ Examples/Sound/Source/main.c
@@ -13,7 +13,9 @@ const float notes[] = {16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 23.12, 24.50, 2
 
 const char * HIGGHWAY_TO_HELL = "8e1 4- 4- 4- 4- 4d2 4d2 8d2 4- 4- 4- 4- 4#c2 4#c2 8#c2 4- 4- 4- 4- 4g2 4e2 4d2 4b1 4b1 4a1 4g1 8e1";
 
-char * ACTUAL_SONG = "8e1 4- 4- 4- 4- 4d2 4d2 8d2 4- 4- 4- 4- 4#c2 4#c2 8#c2 4- 4- 4- 4- 4g2 4e2 4d2 4b1 4b1 4a1 4g1 8e1";
+char * MAMA_MIA = "32f2 32#d2 32f2 8#d2 32#d2 32#d2 32f2 32g2 32f2 16.#d2 32- 16f2 8#d2 16#g2 32#g2 32#g2 32#g2 16g2 16.#d232- 8#a2 32#a2 32#a2 16#a2 16f2 16g2 8#g2 16g2 16g2 32g2 16g2 16d2 16#d2 8f2 16f2 8#d2 16#g2 32#g2 32#g232#g2 32g2 32#d2 32f2 16#d2";
+
+char * ACTUAL_SONG = MAMA_MIA;
 
 int split (const char *str, char c, char ***arr)
 {
